1979ce84ab04c473c53241dc80bb4cf07ee604c0
vickisaki <victorhugotds23gmail.com>
2026-04-12 13:28:53 -0300

    Iniciando com Teste Funcionais

--- OneDrive/Documentos/tdd-project/functional_tests.py
+++ OneDrive/Documentos/tdd-project/functional_tests.py
@@ -1,7 +1,38 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get("http://127.0.0.1:8000/")
+class NewVisitorTest(unittest.TestCase):
 
-assert "Congratulations!" in browser.title
-print("OK")
+    def test_can_start_a_list_and_retrieve_it_later(self): 
+    
+        self.browser = webdriver.Firefox()
+    
+        # Maria decidiu utilizar o novo app TODO. Ela entra em sua página principal:
+        self.browser.get('http://localhost:8000')
+
+        # Ela nota que o título da página menciona TODO
+        self.assertIn('To-Do', self.browser.title)
+
+        # Ela é convidada a entrar com um item TODO imediatamente
+
+        # Ela digita "Estudar testes funcionais" em uma caixa de texto
+
+        # Quando ela aperta enter, a página atualiza, e mostra a lista
+        # "1: Estudar testes funcionais" como um item da lista TODO
+        
+        # Ainda existe uma caixa de texto convidando para adicionar outro item
+        # Ela digita: "Estudar testes de unidade"
+
+        # A página atualiza novamente, e agora mostra ambos os itens na sua lista
+
+        # Maria se pergunta se o site vai lembrar da sua lista. Então, ela verifica que
+        # o site gerou uma URL única para ela -- existe uma explicação sobre essa feature
+
+        # Ela visita a URL: a sua lista TODO ainda está armazenada
+
+        # Satisfeita, ela vai dormir
+        
+        self.browser.quit()
+
+if __name__ == '__main__':
+    unittest.main()
